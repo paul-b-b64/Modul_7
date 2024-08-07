aa1dc2bd3d91b2eea27cab4bf5b3c54cc2ffc54d
paul-b-b64 <belousko@inbox.ru>
2024-08-07 19:33:05 +0300

    ДЗ 7.2

---
+++ Modul_7_3.py
@@ -0,0 +1,50 @@
+class WordsFinder:
+    file_names = []
+    def __init__(self, *args):
+        for i in args:
+            self.file_names.append(i)
+
+    def get_all_words(self):
+        all_words = {}
+        punktuations = [',', '.', '=', '!', '?', ';', ':', ' - ']
+        for i in self.file_names:
+            with open(i, encoding='utf-8') as file:
+                words_in_file = []
+                for line in file:
+                    str_ = line.lower()
+                    for j in str_:
+                        if j in punktuations:
+                            str_ = str_.replace(j, '')
+                        list_of_words = str_.split()
+                    for k in list_of_words:
+                        words_in_file.append(k)
+                all_words[i] = words_in_file
+        return all_words
+
+    def find(self, word):
+        find_word_first_position = {}
+        dict_1 = self.get_all_words()
+        low_word = word.lower()
+        for name, words in dict_1.items():
+            if low_word in dict_1[name]:
+                find_word_first_position[name] = dict_1[name].index(low_word) + 1
+        return find_word_first_position
+
+
+    def count(self, word):
+        count_word = {}
+        dict_1 = self.get_all_words()
+        low_word = word.lower()
+        for name, words in dict_1.items():
+            if word.lower() in dict_1[name]:
+                count_word[name] = dict_1[name].count(low_word)
+        return count_word
+
+
+
+finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
+                      'Rudyard Kipling - If.txt',
+                      'Mother Goose - Monday’s Child.txt')
+print(finder1.get_all_words())
+print(finder1.find('the'))
+print(finder1.count('the'))
